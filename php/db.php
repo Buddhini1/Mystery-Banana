a693cf19e472821762be0ac8a7412e817fa2dfda
Buddhini <buddhinialwis@gmail.com>
2025-04-03 18:32:20 +0530

    Update db.php

--- php/db.php
+++ php/db.php
@@ -4,11 +4,11 @@
 $host = "localhost";
 $user = "root";  
 $password = "";  
-$database = "banana_game";
+$database = "mysterybanana"; // Database name
 
-$conn = new mysqli($host, $user, $password, $database);
+$conn = new mysqli($host, $user, $password, $database); // Create connection
 
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    die("Connection failed: " . $conn->connect_error); // Handle connection error
 }
 ?>
